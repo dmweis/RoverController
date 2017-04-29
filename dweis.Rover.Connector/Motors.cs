292e52571fa7a7af326155730ba23adf4710744a
David Weis <davidmichaelweis@gmail.com>
2017-04-29 19:38:19 +0200

    Update Motor enums to reflect new configuration

--- dweis.Rover.Connector/Motors.cs
+++ dweis.Rover.Connector/Motors.cs
@@ -3,13 +3,14 @@ namespace dweis.Rover.Connector
 {
    public enum Motors
    {
-      LEFT_FRONT_MOTOR = 0,
-      LEFT_FRONT_SERVO = 1,
-      LEFT_REAR_SERVO = 2,
-      RIGHT_FRONT_MOTOR = 3,
-      RIGHT_REAR_MOTOR = 4,
-      RIGHT_REAR_SERVO = 5,
-      RIGHT_FRONT_SERVO = 6,
-      LEFT_REAR_MOTOR = 17
+      LEFT_FRONT_SERVO  = 0,
+      RIGHT_FRONT_SERVO = 1,
+      LEFT_REAR_SERVO   = 2,
+      RIGHT_REAR_SERVO  = 3,
+
+      LEFT_FRONT_MOTOR  = 4,
+      RIGHT_FRONT_MOTOR = 5,
+      LEFT_REAR_MOTOR   = 6,
+      RIGHT_REAR_MOTOR  = 7
    }
 }
